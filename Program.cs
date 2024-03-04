c0cb67e39cfed0e956f056e18f352ed9cdd39d77
angelostefani78 <angelostefani78@gmail.com>
2024-03-04 12:26:58 +0100

    Predisposizione per primo deploy su Venere

--- Program.cs
+++ Program.cs
@@ -5,24 +5,26 @@ using Microsoft.JSInterop;
 using MultiClimact.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
 
+//LOCALIZZAZIONE
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
-// Add services to the container.
+builder.Services.AddRazorPages()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
+
+// DATABASE
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+//AUTENTICAZIONE
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages()
-    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
-    .AddDataAnnotationsLocalization();
 
 var app = builder.Build();
-
-//ConfigurationManager configuration = builder.Configuration;
 
 var supportedCultures = new[] { "en", "it" };
 var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
@@ -48,6 +50,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
