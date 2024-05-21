0e7870ad32d26b7c1bc3f73e6ff44903c31a7232
Angelo Stefani <40657108+angelostefani78@users.noreply.github.com>
2024-05-21 12:38:19 +0200

    New menu management

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -85,22 +85,93 @@ namespace MultiClimact.Pages
             }
 
             // Altri dati da passare alla vista
-            ViewData["wmsurl1"] = _configuration["wms:wmsurl1"];
-            ViewData["wmslayer1"] = _configuration["wms:wmslayer1"];
+            ViewData["wmsurl_mapA1"] = _configuration["wms:wmsurl_mapA1"];
+            ViewData["wmslayer_mapA1"] = _configuration["wms:wmslayer_mapA1"];
 
-            ViewData["wmsurl2"] = _configuration["wms:wmsurl2"];
-            ViewData["wmslayer2"] = _configuration["wms:wmslayer2"];
+            ViewData["wmsurl_mapA2"] = _configuration["wms:wmsurl_mapA2"];
+            ViewData["wmslayer_mapA2"] = _configuration["wms:wmslayer_mapA2"];
 
-            ViewData["wmsurl3"] = _configuration["wms:wmsurl3"];
-            ViewData["wmslayer3"] = _configuration["wms:wmslayer3"];
+            ViewData["wmsurl_mapA3"] = _configuration["wms:wmsurl_mapA3"];
+            ViewData["wmslayer_mapA3"] = _configuration["wms:wmslayer_mapA3"];
 
-            ViewData["wmsurl4"] = _configuration["wms:wmsurl4"];
-            ViewData["wmslayer4"] = _configuration["wms:wmslayer4"];
+            ViewData["wmsurl_mapA4"] = _configuration["wms:wmsurl_mapA4"];
+            ViewData["wmslayer_mapA4"] = _configuration["wms:wmslayer_mapA4"];
 
-            ViewData["wmsurl5"] = _configuration["wms:wmsurl5"];
-            ViewData["wmslayer5"] = _configuration["wms:wmslayer5"];
+            ViewData["wmsurl_mapB1"] = _configuration["wms:wmsurl_mapB1"];
+            ViewData["wmslayer_mapB1"] = _configuration["wms:wmslayer_mapB1"];
 
-            // ... (Altri dati)
+            ViewData["wmsurl_mapB2"] = _configuration["wms:wmsurl_mapB2"];
+            ViewData["wmslayer_mapB2"] = _configuration["wms:wmslayer_mapB2"];
+
+            ViewData["wmsurl_mapB3"] = _configuration["wms:wmsurl_mapB3"];
+            ViewData["wmslayer_mapB3"] = _configuration["wms:wmslayer_mapB3"];
+
+            ViewData["wmsurl_mapB4"] = _configuration["wms:wmsurl_mapB4"];
+            ViewData["wmslayer_mapB4"] = _configuration["wms:wmslayer_mapB4"];
+
+            ViewData["wmsurl_mapB5"] = _configuration["wms:wmsurl_mapB5"];
+            ViewData["wmslayer_mapB5"] = _configuration["wms:wmslayer_mapB5"];
+
+            ViewData["wmsurl_mapB6"] = _configuration["wms:wmsurl_mapB6"];
+            ViewData["wmslayer_mapB6"] = _configuration["wms:wmslayer_mapB6"];
+
+           
+
+            ViewData["wmsurl_mapC1"] = _configuration["wms:wmsurl_mapC1"];
+            ViewData["wmslayer_mapC1"] = _configuration["wms:wmslayer_mapC1"];
+
+            ViewData["wmsurl_mapC2"] = _configuration["wms:wmsurl_mapC2"];
+            ViewData["wmslayer_mapC2"] = _configuration["wms:wmslayer_mapC2"];
+
+            ViewData["wmsurl_mapC3"] = _configuration["wms:wmsurl_mapC3"];
+            ViewData["wmslayer_mapC3"] = _configuration["wms:wmslayer_mapC3"];
+
+            ViewData["wmsurl_mapC4"] = _configuration["wms:wmsurl_mapC4"];
+            ViewData["wmslayer_mapC4"] = _configuration["wms:wmslayer_mapC4"];
+
+            ViewData["wmsurl_mapC5"] = _configuration["wms:wmsurl_mapC5"];
+            ViewData["wmslayer_mapC5"] = _configuration["wms:wmslayer_mapC5"];
+
+            ViewData["wmsurl_mapC6"] = _configuration["wms:wmsurl_mapC6"];
+            ViewData["wmslayer_mapC6"] = _configuration["wms:wmslayer_mapC6"];
+
+            ViewData["wmsurl_mapC7"] = _configuration["wms:wmsurl_mapC7"];
+            ViewData["wmslayer_mapC7"] = _configuration["wms:wmslayer_mapC7"];
+
+            ViewData["wmsurl_mapC8"] = _configuration["wms:wmsurl_mapC8"];
+            ViewData["wmslayer_mapC8"] = _configuration["wms:wmslayer_mapC8"];
+
+            ViewData["wmsurl_mapC9"] = _configuration["wms:wmsurl_mapC9"];
+            ViewData["wmslayer_mapC9"] = _configuration["wms:wmslayer_mapC9"];
+
+            ViewData["wmsurl_mapC10"] = _configuration["wms:wmsurl_mapC10"];
+            ViewData["wmslayer_mapC10"] = _configuration["wms:wmslayer_mapC10"];
+
+            ViewData["wmsurl_mapC11"] = _configuration["wms:wmsurl_mapC11"];
+            ViewData["wmslayer_mapC11"] = _configuration["wms:wmslayer_mapC11"];
+
+            ViewData["wmsurl_mapC12"] = _configuration["wms:wmsurl_mapC12"];
+            ViewData["wmslayer_mapC12"] = _configuration["wms:wmslayer_mapC12"];
+
+            ViewData["wmsurl_mapC13"] = _configuration["wms:wmsurl_mapC13"];
+            ViewData["wmslayer_mapC13"] = _configuration["wms:wmslayer_mapC13"];
+
+            ViewData["wmsurl_mapD1"] = _configuration["wms:wmsurl_mapD1"];
+            ViewData["wmslayer_mapD1"] = _configuration["wms:wmslayer_mapD1"];
+
+            ViewData["wmsurl_mapD2"] = _configuration["wms:wmsurl_mapD2"];
+            ViewData["wmslayer_mapD2"] = _configuration["wms:wmslayer_mapD2"];
+
+            ViewData["wmsurl_mapD3"] = _configuration["wms:wmsurl_mapD3"];
+            ViewData["wmslayer_mapD3"] = _configuration["wms:wmslayer_mapD3"];
+
+            ViewData["wmsurl_mapD4"] = _configuration["wms:wmsurl_mapD4"];
+            ViewData["wmslayer_mapD4"] = _configuration["wms:wmslayer_mapD4"];
+
+            ViewData["wmsurl_mapD5"] = _configuration["wms:wmsurl_mapD5"];
+            ViewData["wmslayer_mapD5"] = _configuration["wms:wmslayer_mapD5"];
+            
+           
         }
 
         public async Task<IActionResult> OnPostAsync()
@@ -121,19 +192,7 @@ namespace MultiClimact.Pages
                 Radius = int.Parse(Request.Form["radius"]);
 
                 var earthquake = new
-                {
-                     
-                    /*lon = 42.9087,
-                    lat = 13.1288,
-                    description = "visso test",
-                    damageLaw = 4,
-                    pgaLaw = 5,
-                    depth = 6.0,
-                    magnitude = 5.0,
-                    fault = 0,
-                    options = "100000111",
-                    radius = 30*/
-
+                {                 
                     lon = Lon,
                     lat = Lat,
                     description = Description,
@@ -144,7 +203,6 @@ namespace MultiClimact.Pages
                     fault = Fault,
                     options = Options,
                     radius = Radius
-
                 };
 
                 var userPlatform = new
@@ -169,7 +227,6 @@ namespace MultiClimact.Pages
                 System.Console.WriteLine("Fault: " + Fault);
                 System.Console.WriteLine("Options: " + Options);
                 System.Console.WriteLine("Radius: " + Radius);
-
                
                 var client = new HttpClient();
                 var request = new HttpRequestMessage(HttpMethod.Post, apiUrl);
@@ -204,6 +261,5 @@ namespace MultiClimact.Pages
                 return Page(); // Stay on the same page
             }
         }
-
     }
 }
