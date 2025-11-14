556816196a5a7a83c79534a1dd9fc5262ac5b4a3
jacopo1010 <166029437+jacopo1010@users.noreply.github.com>
2025-11-14 14:34:00 +0100

    Finita E4!! in funzione anche il WS10

--- Controllers/ConfigVulnProxyController.cs
+++ Controllers/ConfigVulnProxyController.cs
@@ -31,22 +31,42 @@ namespace MultiClimact.Controllers
                 this.configuration = configuration;
            }
 
-           [HttpGet("ws9")]
-           public async Task<IActionResult> GetWs9(){
-               var user_id = User.FindFirstValue(System.Security.Claims.ClaimTypes.NameIdentifier) ?? User.FindFirstValue("sub") ?? User.FindFirstValue("oid");
-                logger.LogInformation("Fetching vulnerability configs for user {user_id}", user_id);
-                var baseUrl = this.configuration["ConfigVulnService:BaseUrl"];
-                var url = $"{baseUrl}/users/system/configurations";
-                logger.LogInformation("Requesting ConfigVulnService at {url}", url);
-                var response = await http.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-                //{Uri.EscapeDataString(user_id)}
-                var content = await response.Content.ReadAsStringAsync();
-                logger.LogInformation("Received response from ConfigVulnService: {Content}", content);
-                return Content(content, "application/json");
-           }
+        [HttpGet("ws9")]
+        public async Task<IActionResult> GetWs9()
+        {
+            var user_id = User.FindFirstValue(System.Security.Claims.ClaimTypes.NameIdentifier) ?? User.FindFirstValue("sub") ?? User.FindFirstValue("oid");
+            logger.LogInformation("Fetching vulnerability configs for user {user_id}", user_id);
+            var baseUrl = this.configuration["ConfigVulnService:BaseUrl"];
+            var url = $"{baseUrl}/users/system/configurations";
+            logger.LogInformation("Requesting ConfigVulnService at {url}", url);
+            var response = await http.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            //{Uri.EscapeDataString(user_id)}
+            var content = await response.Content.ReadAsStringAsync();
+            logger.LogInformation("Received response from ConfigVulnService: {Content}", content);
+            return Content(content, "application/json");
+        }
+
+
+
+        [HttpPut("ws10")]
+        public async Task<IActionResult> PutWs10([FromBody] JsonElement configData)
+        {
+            var user_id = User.FindFirstValue(System.Security.Claims.ClaimTypes.NameIdentifier) ?? User.FindFirstValue("sub") ?? User.FindFirstValue("oid");
+            logger.LogInformation("Updating vulnerability configs for user {user_id}", user_id);
+            var baseUrl = this.configuration["ConfigVulnService:BaseUrl"];
+            var url = $"{baseUrl}/users/system/configurations/default";
+            //{Uri.EscapeDataString(user_id)}
+            logger.LogInformation("Sending update to ConfigVulnService at {url}", url);
+            var jsonString = configData.GetRawText();
+            var content = new StringContent(jsonString, System.Text.Encoding.UTF8, "application/json");
+            var response = await http.PutAsync(url, content);
+            var responseContent = await response.Content.ReadAsStringAsync();
+             if (!response.IsSuccessStatusCode)
+             return StatusCode((int)response.StatusCode, string.IsNullOrWhiteSpace(responseContent) ? "WS10 error" : responseContent);
+            logger.LogInformation("Received response from ConfigVulnService: {ResponseContent}", responseContent);
+            return Content(responseContent, "application/json");
+        }
     }
-
-
 
 }
